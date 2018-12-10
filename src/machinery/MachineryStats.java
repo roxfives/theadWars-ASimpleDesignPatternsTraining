66de9d24e06147de3864d34c719f0a2b9e4c66e8
rchagas <ricardo.chagas2@outlook.com>
2018-12-10 08:47:01 -0200

    Corrects the firepower of the war units

--- src/machinery/MachineryStats.java
+++ src/machinery/MachineryStats.java
@@ -5,13 +5,13 @@ package machinery;
  */
 public class MachineryStats {
     protected static final double PLANE_LIFE = 100.0;
-    protected static final int PLANE_FIRE_POWER = 10;
+    protected static final int PLANE_FIRE_POWER = 20;
 
     protected static final double KAMIKAZE_LIFE = 1.0;
-    protected static final int KAMIKAZE_FIRE_POWER = 100;
+    protected static final int KAMIKAZE_FIRE_POWER = 80;
 
     protected static final double BASE_LAND_LIFE = 1000.0;
-    protected static final int BASE_LAND_FIRE_POWER = 10;
+    protected static final int BASE_LAND_FIRE_POWER = 30;
 
     protected static final double SHIP_LIFE = 1000.0;
     protected static final int SHIP_FIRE_POWER = 100;
